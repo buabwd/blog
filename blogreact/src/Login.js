efd96cc07569096360f0bd3616e42dbabd6b061e
Teeranai <teeranai.kon@gmail.com>
2021-03-26 00:10:08 +0700

    Remove duplicate CSS

--- blogreact/src/Login.js
+++ blogreact/src/Login.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import './CSS/Login.css';
-
+import React from "react";
+import "./CSS/Login.css";
 
 const Login = () => {
-    return (
-        <div>
-            <div class="center">
-                <h1>Login</h1>
-                <form method="post"></form>
-                <div class="txt_field">
-                    <input type="text" required></input>
-                        <span></span>
-                        <label>Username</label>
+  return (
+    <div style={{ height: "100vh", overflow: "hidden" }}>
+      <div class="center">
+        <h1>Login</h1>
+        <form method="post"></form>
+        <div class="txt_field">
+          <input type="text" required></input>
+          <span></span>
+          <label>Username</label>
         </div>
-                    <div class="txt_field">
-                        <input type="password" required></input>
-                            <span></span>
-                            <label>Password</label>
+        <div class="txt_field">
+          <input type="password" required></input>
+          <span></span>
+          <label>Password</label>
         </div>
-                        <div class="pass">Forget Password?</div>
-                        <input type="submit" value="Login"></input>
-                            <div class="signup-link">
-                                Not a member?<a href="#">Signup</a>
-                            </div>
+        <div class="pass">Forget Password?</div>
+        <input type="submit" value="Login"></input>
+        <div class="signup-link">
+          Not a member?<a href="#">Signup</a>
+        </div>
+      </div>
     </div>
-                    </div>
-    )
-}
+  );
+};
 
-export default Login
+export default Login;
